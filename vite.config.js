774b07d6de9920d4c749d13f5824f74e31a21ab2
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-03-16 19:35:58 +0300

    add vite mksert config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 export default {
   server: {
-    port: 8088
-  }
-}
+    port: 8888,
+    host: "local.dev",
+    https: true,
+  },
+  plugins: [mkcert()],
+};
