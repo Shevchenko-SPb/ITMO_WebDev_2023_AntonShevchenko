324d4ce838ace71d41bc21bdc01013fc76e4e670
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-03-21 22:17:03 +0300

    lesson

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
 export default {
   server: {
     port: 8888,
-    host: "local.dev",
-    https: true,
   },
-  plugins: [mkcert()],
-  build: {},
 };
