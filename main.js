e15edf4f3c7d0a36f9dc57b95b34d8ddd6ce94b5
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-01 13:09:12 +0300

    End lesson

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-import "uno.css"
-import '@unocss/reset/tailwind.css'
+const DOM = document.getElementById.bind(document)
+
+const domInpName = DOM('inpName');
+const domInpSurname = DOM('inpSurname');
+const domConResult = DOM('conResult');
+
+let fullname = "";
+
+domInpName.oninput = function(event) {
+  console.log("omInpNameChange:", { event });
+  renderFullname()
+}
+domInpSurname.oninput = onInpSurnameInput;
+
+function renderFullname () {
+  fullname = domInpName.value + " " + domInpSurname.value;
+  domConResult.textContent = fullname;
+}
+function onInpNameInput(event) {
+  console.log("omInpNameChange:", { event });
+  renderFullname()
+}
+function onInpSurnameInput(event) {
+  console.log("omInpSurnameChange:", { event });
+  renderFullname()
+}
+
+console.log(domInpName, domInpSurname);
