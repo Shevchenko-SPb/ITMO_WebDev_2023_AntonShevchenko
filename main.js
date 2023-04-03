ee210b5e6fe85e8ac90041d4e47df316c0a4439b
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-04 00:57:41 +0300

    1

--- main.js
+++ main.js
@@ -1,61 +1,48 @@
+
 const DOM = document.getElementById.bind(document);
 
-const domFirstNumber = DOM("inpFirstNumber");
+const domInpFirstNumber = DOM("inpFirstNumber");
 const domInpSecondNumber = DOM("inpSecondNumber");
 const domResultCount = DOM("ResultCount");
-const domBtn1 = DOM("bt1");
+const domBtnResult = DOM("btnResult");
+const domBtnSummarize = DOM("btnSummarize");
+const domBtnSubtract = DOM("btnSubtract");
+const domBtnActionSymbol = DOM("actionSymbol")
+const domBtn1 = DOM("btn1");
+const domBtn2 = DOM("btn2");
+const domBtn3 = DOM("btn3");
+const domBtn4 = DOM("btn4");
+const domBtn5 = DOM("btn5");
+const domBtn6 = DOM("btn6");
+const domBtn7 = DOM("btn7");
+const domBtn8 = DOM("btn8");
+const domBtn9 = DOM("btn9");
 
-domBtn1.onclick = button1(15,4)
-  function button1 (a, b) {
-  const result = a + b;
-  console.log('sum:', { a, b, result });
+let calResult = "";
+
+
+domBtnResult.onclick = onBtnResult;
+domBtnSummarize.onclick = onBtnSummarize;
+domBtnSubtract.onclick = onBtnSubtract;
+
+
+function onBtnSummarize () {
+  let num1 = Number(domInpFirstNumber.value);
+  let num2 = Number(domInpSecondNumber.value);
+  domBtnActionSymbol.innerHTML = "+";
+  calResult = num1 + num2;
+}
+function onBtnSubtract () {
+  let num1 = Number(domInpFirstNumber.value);
+  let num2 = Number(domInpSecondNumber.value);
+  domBtnActionSymbol.innerHTML = "-";
+  calResult = num1 - num2;
+}
+function onBtnResult () {
+  domResultCount.innerHTML = calResult;
 }
 
-function sum(a, b) {
-  const result = a + b;
-  console.log('sum:', { a, b, result });
-
-}
-
-
-domFirstNumber.oninput = sum(3,4)
-domInpSecondNumber.oninput = sum(5,55)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// domInpName.oninput = function (event) {
-//   console.log("onInpNameInput:", { event });
-//   renderFullName();
-// };
-//
-// domInpSurname.oninput = function (event) {
-//   console.log("onInpNameInput:", { event });
-//   renderFullName();
-// };
-//
-// const getFullName = () => `${domInpName.value} ${domInpSurname.value}`;
-//
-// function renderFullName() {
-//   const fullName = getFullName();
-//   console.log("renderFullName:", { fullName });
-//   domConResult.textContent = fullName;
-// }
-//
-// console.log(domInpName, domInpSurname);
