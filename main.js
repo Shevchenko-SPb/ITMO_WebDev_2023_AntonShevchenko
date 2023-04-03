6af294c244527d19231d0f058aa51c2cfdd7065f
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-03 16:17:59 +0300

    .

--- main.js
+++ main.js
@@ -1,25 +1,61 @@
 const DOM = document.getElementById.bind(document);
 
-const domInpName = DOM("inpName");
-const domInpSurname = DOM("inpSurname");
-const domConResult = DOM("conResult");
+const domFirstNumber = DOM("inpFirstNumber");
+const domInpSecondNumber = DOM("inpSecondNumber");
+const domResultCount = DOM("ResultCount");
+const domBtn1 = DOM("bt1");
 
-domInpName.oninput = function (event) {
-  console.log("onInpNameInput:", { event });
-  renderFullName();
-};
-
-domInpSurname.oninput = function (event) {
-  console.log("onInpNameInput:", { event });
-  renderFullName();
-};
-
-const getFullName = () => `${domInpName.value} ${domInpSurname.value}`;
-
-function renderFullName() {
-  const fullName = getFullName();
-  console.log("renderFullName:", { fullName });
-  domConResult.textContent = fullName;
+domBtn1.onclick = button1(15,4)
+  function button1 (a, b) {
+  const result = a + b;
+  console.log('sum:', { a, b, result });
 }
 
-console.log(domInpName, domInpSurname);
+function sum(a, b) {
+  const result = a + b;
+  console.log('sum:', { a, b, result });
+
+}
+
+
+domFirstNumber.oninput = sum(3,4)
+domInpSecondNumber.oninput = sum(5,55)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// domInpName.oninput = function (event) {
+//   console.log("onInpNameInput:", { event });
+//   renderFullName();
+// };
+//
+// domInpSurname.oninput = function (event) {
+//   console.log("onInpNameInput:", { event });
+//   renderFullName();
+// };
+//
+// const getFullName = () => `${domInpName.value} ${domInpSurname.value}`;
+//
+// function renderFullName() {
+//   const fullName = getFullName();
+//   console.log("renderFullName:", { fullName });
+//   domConResult.textContent = fullName;
+// }
+//
+// console.log(domInpName, domInpSurname);
