020a0427c9b7b2df39a8e712a73b5bf0568294b1
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-05 10:31:16 +0300

    grids

--- main.js
+++ main.js
@@ -12,19 +12,19 @@ const domResultSymbol = DOM("resultSymbol");
 const domBtnMultiply = DOM("btnMultiply");
 const domBtnDivide = DOM("btnDivide");
 const domBtnReset = DOM("btnReset");
-const domBtn1 = DOM("btn1");
-const domBtn2 = DOM("btn2");
-const domBtn3 = DOM("btn3");
-const domBtn4 = DOM("btn4");
-const domBtn5 = DOM("btn5");
-const domBtn6 = DOM("btn6");
-const domBtn7 = DOM("btn7");
-const domBtn8 = DOM("btn8");
-const domBtn9 = DOM("btn9");
-const domRadioBtn1 = DOM("RadioBtn1")
+
+let domButtons = document.getElementsByClassName("btn");
+
+domButtons.click = (e) => console.log(domButtons.value)
+
+
+
+
+
+
 
 let calResult = "";
-let selectedInput = domInpFirstNumber;
+let selectedInput;
 
 domBtnResult.onclick = onBtnResult;
 domBtnSummarize.onclick = onBtnSummarize;
@@ -32,17 +32,12 @@ domBtnSubtract.onclick = onBtnSubtract;
 domBtnMultiply.onclick = onBtnMultiply;
 domBtnDivide.onclick = onBtnDivide;
 domBtnReset.onclick = onBtnReset;
-domBtn1.onclick = onbtn1;
-domBtn2.onclick = onBtn2;
-domBtn3.onclick = onbtn3;
-domBtn4.onclick = onbtn4;
-domBtn5.onclick = onbtn5;
-domBtn6.onclick = onbtn6;
-domBtn7.onclick = onbtn7;
-domBtn8.onclick = onbtn8;
-domBtn9.onclick = onbtn9;
-domInpFirstNumber.click = (e) => selectedInput = domInpFirstNumber;
-domInpSecondNumber.click = (e) => selectedInput = domInpSecondNumber;
+
+
+domInpFirstNumber.click = (e) => selectedInput = domInpFirstNumber ;
+domInpSecondNumber.click = (e) => selectedInput = domInpSecondNumber ;
+
+
 
 function onBtnSummarize () {
   let num1 = Number(domInpFirstNumber.value);
@@ -77,11 +72,4 @@ function onBtnReset () {
   domInpSecondNumber.value = "";
   domResultCount.innerHTML = "";
 }
-function onbtn1 () {
-  if (domRadioBtn1.value = "option1"){
-  domInpFirstNumber.value += 1 }
-  else {
-  domInpSecondNumber.value += 1 }
-}
-
 
