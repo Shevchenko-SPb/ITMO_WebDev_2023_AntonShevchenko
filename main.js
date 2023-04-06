45db6443b22d4f82c7d2e7032be2bd34405c932c
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-06 12:46:44 +0300

    progress

--- main.js
+++ main.js
@@ -1,41 +1,28 @@
 
 const appendBlock = (block) => document.getElementById("app").appendChild(block);
-
-const createBlock = (x, y, size, color) => {
+const createBlock = (x, y, size = 50) => {
   const result = document.createElement("div");
-  if(color) {
-    result.style.backgroundColor = color;
-  }else{
-    const isEmpty = Math.random() > 0.5;
-    if(isEmpty) {
-      result.style.backgroundColor = "black";
-    }
+  const isEmpty = Math.random() > 0.5;
+  if (isEmpty) {
+  } else {
+    result.style.backgroundColor = "black";
   }
-  result.style.background = "black";
-  result.style.width = "50px";
-  result.style.height = "50px";
+  result.style.width = result.style.height = `${size}px`;
   result.style.position = "absolute";
-  result.style.left =
+  result.style.left = `${x}px`;
+  result.style.top = `${y}px`;
   return result;
-}
+};
 
 const BLOCK_SIZE = 50;
-
-let columns = 6;
-let xPos = 0;
-let yPos = 0;
-
-const line = [];
-
+let columns = 5;
+let xPos = 100;
+let yPos = 100;
 
 while(columns-- > 0) {
-  const block = createBlock(xPos, yPos, BLOCK_SIZE);
-  line.push(block);
-  xPos += BLOCK_SIZE;
+  const block = createBlock (xPos, yPos, BLOCK_SIZE);
+  xPos = xPos + BLOCK_SIZE;
   appendBlock(block);
 }
 
-line.reverse().forEach((block) => {
-  xPos += BLOCK_SIZE;
 
-})
