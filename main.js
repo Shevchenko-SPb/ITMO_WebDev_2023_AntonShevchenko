ca4c14186f95fffee6dc3709809e67423141ac17
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-06 22:00:49 +0300

    End lesson 06.04

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import "uno.css"
 import '@unocss/reset/tailwind.css'
+
+const domBtnCreateTask = document.getElementById("btnCreateTask");
+const domPopupCreateTask = document.getElementById("popupCreateTask");
+const domBtnCloseCreateTaskPopup = document.getElementById("btnCloseCreateTaskPopup");
+
+domBtnCreateTask.onclick = (e) => {
+  console.log(e);
+  domPopupCreateTask.classList.remove("hidden");
+
+  domBtnCloseCreateTaskPopup.onclick = () => {
+    domPopupCreateTask.classList.add("hidden");
+    domBtnCloseCreateTaskPopup.onclick = null;
+  }
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./main.js",
+    "./src/**/*.{js,ts,jsx,tsx,}",
   ],
   theme: {
     extend: {},
