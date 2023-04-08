17600f47ec3acbaed426ebd9a2fa83028d96c0d6
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-08 13:18:19 +0300

    End Lesson 08.04

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-import "uno.css"
-import '@unocss/reset/tailwind.css'
+import "uno.css";
+import "@unocss/reset/tailwind.css";
+import DOM from "./src/constants/dom";
+import { randomString } from "./src/utils/stringUtils.js";
 
-const domBtnCreateTask = document.getElementById("btnCreateTask");
-const domPopupCreateTask = document.getElementById("popupCreateTask");
-const domBtnCloseCreateTaskPopup = document.getElementById("btnCloseCreateTaskPopup");
 
-domBtnCreateTask.onclick = (e) => {
-  console.log(e);
-  domPopupCreateTask.classList.remove("hidden");
-
-  domBtnCloseCreateTaskPopup.onclick = () => {
-    domPopupCreateTask.classList.add("hidden");
-    domBtnCloseCreateTaskPopup.onclick = null;
+const Tags = ["Web","Update","Design","Content"];
+class TaskVO {
+  constructor (title, date, tag) {
+    this.title = title;
+    this.data = date;
+    this.tag = tag;
   }
+}
+
+const getDOM = (id) => document.getElementById(id);
+const QUERY = (container, id) => container.querySelector(`[data-id="${id}"]`);
+
+const domTask = getDOM(DOM.Template.TASK);
+const tasks = [];
+
+getDOM(DOM.Button.CREATE_TASK).onclick = () => {
+  console.log("> domPopupCreateTask.classList");
+
+  const domPopupCreateTask = getDOM(DOM.Popup.CREATE_TASK);
+  const domBtnClose = QUERY(domPopupCreateTask, DOM.Button.CLOSE_POPUP_CREATE_TASK);
+  const domBtnCreate = QUERY(domPopupCreateTask, DOM.Button.POPUP_CREATE_TASK_CONFIRM);
+
+  domPopupCreateTask.classList.remove("hidden");
+  domBtnClose.onclick = () => {
+    domPopupCreateTask.classList.add("hidden");
+    domBtnClose.onclick = null;
+  };
+  domBtnCreate.onclick = () => {
+    const taskVO = new TaskVO(randomString(12), Date.now(), Tags[0])
+    const taskView = domTask.cloneNode(true)
+    QUERY(taskView, DOM.Template.Task.TITLE).innerText = taskVO.title;
+    domTask.parentNode.prepend(taskView)
+    tasks.push(TaskVO);
+  };
 };
---
+++ src/utils/stringUtils.js
@@ -0,0 +1,4 @@
+const randomString = (len, chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') =>
+  [...Array(len)].map(() => chars.charAt(Math.floor(Math.random() * chars.length))).join('')
+
+export {randomString}
