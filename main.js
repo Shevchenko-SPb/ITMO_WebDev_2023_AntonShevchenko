690719e1293bf9f2d860432360fe889d153fe4d9
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-04-11 00:23:46 +0300

    End Video Lesson

--- main.js
+++ main.js
@@ -1,16 +1,57 @@
 import "uno.css";
 import "@unocss/reset/tailwind.css";
+import Dom from "./src/constants/dom.js";
+import { presetTagify } from "unocss";
+import { randomString } from "./src/utils/stringUtils.js";
 
-const domBtnCreateTask = document.getElementById("btnCreateTask");
-const domPopupCreateTask = document.getElementById("popupCreateTask");
+const Tags = ["Web", "Update", "Design", "Content"];
+class TaskVO {
+  constructor(title, date, tag) {
+  this.title = title;
+  this.date = date;
+  this.tag = tag;
+  }
+}
 
-domBtnCreateTask.onclick = () => {
-  console.log("> domBtnCreateTask", domBtnCreateTask);
+const task = new TaskVO("read", Date.now())
+
+
+const DOM = (id) => document.getElementById(id);
+const QUERY = (container, id) => container.querySelector(`[data-id="${id}"]`);
+
+const domTask = DOM(Dom.Template.TASK);
+
+const tasks = [];
+
+DOM(Dom.Button.CREATE_TASK).onclick = () => {
+  console.log("> domBtnCreateTask");
+
+  const domPopupCreateTask = DOM(Dom.Popup.CREATE_TASK);
+  const domBtnClose = QUERY(domPopupCreateTask, Dom.Button.POPUP_CREATE_TASK_CLOSE);
+  const domBtnConfirm = QUERY(domPopupCreateTask, Dom.Button.POPUP_CREATE_TASK_CONFIRM);
+
   domPopupCreateTask.classList.remove("hidden");
 
-  const domBtnCloseCreateTaskPopup = document.getElementById("btnCloseCreateTaskPopup");
-  domBtnCloseCreateTaskPopup.onclick = () => {
+  const closePopup = () => {
     domPopupCreateTask.classList.add("hidden");
-    domBtnCloseCreateTaskPopup.onclick = null;
+    domBtnClose.onclick = null;
+    domBtnConfirm.onclick = null;
+  };
+
+  domBtnClose.onclick = closePopup;
+
+  domBtnConfirm.onclick = () => {
+    const taskVO = new TaskVO(randomString(10),Date.now(), Tags[0]);
+    const taskView = domTask.cloneNode(true);
+
+    QUERY(taskView, Dom.Template.Task.TITLE).innerText = taskVO.title;
+
+
+    domTask.parentNode.prepend(taskView);
+    tasks.push(taskVO);
+
+
+    console.log("confirm", taskVO);
+    closePopup();
   };
 };
--- src/constants/dom.js
+++ src/constants/dom.js
@@ -1,18 +1,18 @@
-const DOM = {
+const Dom = {
     Button: {
       CREATE_TASK: "btnCreateTask",
-      CLOSE_POPUP_CREATE_TASK: "btnCloseCreateTaskPopup",
+      POPUP_CREATE_TASK_CLOSE: "btnCloseCreateTaskPopup",
       POPUP_CREATE_TASK_CONFIRM: "btnConfirmTaskPopup",
     },
-  Template: {
-      TASK: "templateTask",
-    Task: {
-        TITLE: "templateTaskTitle",
-    }
-  },
+    Template: {
+      TASK: 'templateTask',
+      Task: {
+        TITLE: 'templateTaskTitle',
+      },
+    },
     Popup: {
       CREATE_TASK: "popupCreateTask",
     },
 };
 
-export default DOM;
+export default Dom;
