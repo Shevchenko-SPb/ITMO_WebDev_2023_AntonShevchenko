4b60980729fecdafdd3c4001adfa1687f8ca43dc
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-04 21:55:52 +0300

    1

--- main.js
+++ main.js
@@ -1,60 +1,30 @@
+import { Earth, Planet, Position } from "./src/solar-system.js";
+import { RenderPlanet } from "./renderPlanet.js";
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Planet {
-  x;
-  y;
-  pX;
-  pY;
-  atmosphere;
-  radius;
-  speed;
-  constructor(x, y, speed, size, atmosphere, radius = 50) {
-    this.pX = x;
-    this.pY = y;
-    this.atmosphere = atmosphere;
-    this.radius = radius;
-    this.size = size;
-    this.speed = speed;
-    this.alpha = 0;
-  }
+const centerPosition = new Position(canvas.width / 2, canvas.width / 2);
 
-  move() {
-    planet.x = planet.radius * Math.sin(this.alpha) + planet.pX;
-    planet.y = planet.radius * Math.cos(this.alpha) + planet.pY;
-    this.alpha += (this.speed * Math.PI) / 180;
-  }
-
-  render(ctx) {
-    ctx.beginPath();
-    ctx.fillStyle = this.atmosphere;
-    ctx.arc(this.x, this.y, this.size, 50, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.closePath();
-  }
-}
-
-class Position {
-  x;
-  y;
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
+const sun = new Planet(centerPosition, 0, 50, new RenderPlanet(30, "black"));
+const earth = new Earth(sun.position);
 
 const planets = [
-  new Planet(300, 300, 0.1, 10, "red", 300),
-  new Planet(300, 300, 0.2, 20, "blue", 260),
-  new Planet(300, 300, 0.3, 30, "pink", 200),
-  new Planet(300, 300, 0.4, 40, "green", 130),
+  sun,
+  earth,
+  new Planet(centerPosition, 0.1, 10, new RenderPlanet(40, "yellow")),
+  new Planet(centerPosition, 0.4, 20, new RenderPlanet(25, "red")),
 ];
 
-let planet;
+document.onclick = () => {
+  earth.renderPlanet = new RenderPlanet(10, "blue");
+};
 
+let planet;
+console.log("Planets");
 const render = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
