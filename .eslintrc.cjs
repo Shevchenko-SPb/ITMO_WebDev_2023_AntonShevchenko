fbb36f5946e63d12c3d559d65dad3264a66cfa92
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-06 13:39:11 +0300

    End project

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+  ],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  "vue/max-attributes-per-line": ["error",
+    {
+  "singleline": {
+    "max": 1
+  },
+  "multiline": {
+    "max": 1
+  },
+}]
+
+
+  }
+}
---
+++ src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Enter your name</h1>
+  <div>{{ firstName }} - <span style="color: #535bf2">{{ lastName }}</span></div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  data: () => ({
+    firstName: 'Anton',
+    lastName: 'Shevchenko'
+  })
+};
+</script>
+
+<style scoped>
+
+</style>
