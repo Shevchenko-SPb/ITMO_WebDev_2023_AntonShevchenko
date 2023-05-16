462939a15d6b59066075c8a6fa6282eeeb4bca1f
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-05-16 16:52:24 +0300

    Monsters in massive

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
-const appendBlock = (block) => document.getElementById("app").appendChild(block);
-
-let blocksRaw = [];
-blocksRaw.forEach((element) => console.log(element));
-const generateColor = () => {
-  const isNotEmpty = Math.random() > 0.5;
-  if (isNotEmpty) {
-    return "black";
-  }
-  return null;
-}
-const createBlock = (x, y, size, color) => {
+const container = (xPosMonsters) => {
   const result = document.createElement("div");
-  if (color) {
-    result.style.backgroundColor = color;
+  result.style.left = `${xPosMonsters}px`;
+  const appendBlock = (block) => document.getElementById("app").appendChild(block);
+
+  const generateColor = () => {
+    const isNotEmpty = Math.random() > 0.5;
+    if (isNotEmpty) {
+      return "black";
+    }
+    return null;
+  };
+  const createBlock = (x, y, size, color) => {
+    const result = document.createElement("div");
+    if (color) {
+      result.style.backgroundColor = color;
+    }
+    result.style.width = result.style.height = `${size}px`;
+    result.style.position = "absolute";
+    result.style.left = `${x}px`;
+    result.style.top = `${y}px`;
+    return result;
+  };
+
+  const BLOCK_SIZE = 50;
+  const DIMENTION = 5;
+
+  let columns = DIMENTION;
+  let rows = columns * 2;
+  let xPos = 100;
+  let yPos = 100;
+
+  while (rows-- > 0) {
+    let line = [];
+    xPos = 100;
+    columns = DIMENTION;
+    while (columns-- > 0) {
+      const color = generateColor();
+      const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+      line.push(color);
+      xPos += BLOCK_SIZE;
+      appendBlock(block);
+    }
+    line.reverse().forEach((color) => {
+      const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+      appendBlock(block);
+      xPos += BLOCK_SIZE;
+    });
+    yPos += BLOCK_SIZE;
   }
-  result.style.width = result.style.height = `${size}px`;
-  result.style.position = "absolute";
-  result.style.left = `${x}px`;
-  result.style.top = `${y}px`;
-  return result;
 };
 
-const BLOCK_SIZE = 50;
-const DIMENTION = 5;
 
-let columns = DIMENTION;
-let rows = columns * 2;
-let xPos = 100;
-let yPos = 100;
+let monsters = 4;
+while (monsters-- > 0) {
+  let monsterBlocks = [];
+  let xPosMonsters = 100;
+  xPosMonsters += xPosMonsters;
+  monsterBlocks.push(container(xPosMonsters));
 
-while (rows-- > 0) {
-  let line = [];
-  xPos = 100;
-  columns = DIMENTION;
-  while (columns-- > 0) {
-    const color = generateColor();
-  const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
-  line.push(color);
-  xPos += BLOCK_SIZE;
-  appendBlock(block);
-}
-line.reverse().forEach((color) => {
-  const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
-  appendBlock(block);
-  xPos += BLOCK_SIZE;
-});
-yPos += BLOCK_SIZE;
 }
