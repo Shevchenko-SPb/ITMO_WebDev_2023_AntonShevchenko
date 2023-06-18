22e6f7390ad80242ddd8d70acf9b52f2e61a67e4
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-06-18 22:21:58 +0300

    end popup

--- src/view/popup/TaskPopup.js
+++ src/view/popup/TaskPopup.js
@@ -23,60 +23,76 @@ class TaskPopup {
   render() {
     const div = document.createElement('div');
     div.innerHTML = `
-      <div data-test-id="task-popup" class="flex flex-col relative min-w-[377px] bg-white p-6 rounded-2xl gap-y-4">
-        <button class="absolute top-4 right-4" data-id="btnClose">
-          <i class="i-material-symbols-cancel-outline block text-neutral-400 hover:text-neutral-800 text-2xl"></i>
-        </button>
-        <div class="flex flex-row">
-          <span class="text-xl font-bold">${this.#title}</span>
+    <div class="fixed left-0 top-0 z-10 grid h-screen  w-screen justify-center bg-black/30">
+    <div class="flex  relative flex-col w-screen h-70 bg-neutral-100 max-w-screen-lg">
+      <div class="flex mt-3 flex-col h-full">
+        <div class="flex flex-row mx-7 justify-between h-12">
+          <button class="text-neutral-400 hover:text-neutral-500">Delete</button>
+          <button class="text-neutral-400 hover:text-neutral-500">Close</button>
         </div>
-        <div class="flex flex-row">
-          <div class="flex flex-col w-full">
-            <label class="ml-1 text-sm text-neutral-600" for="inpDate">Title: </label>
-            <input
-              class="bg-neutral-100 p-1.5 rounded w-full border-1 border-neutral-200"
-              data-id="inpTitle"
-              type="text"
-              value="${this.#taskTitle}"
-              placeholder="e.g. Read books"
-            />
+        <div class="flex flex-col mx-7 h-full my-3">
+          <div class="flex flex-row font-bold">
+            <div class="flex flex-row w-1/2 justify-between">
+              <div class="text-4xl">Add</div>
+              <div class="flex flex-row gap-x-1">
+                <div class="flex flex-col justify-between">
+                  <div></div>
+                  <div>Qty:</div>
+                </div>
+                <div class="flex flex-col justify-between">
+                  <div></div>
+                  <input class="h-6 w-16  bg-neutral-100 border-b-2" style="outline:none"></div>
+              </div>
+              <div class="flex"></div>
+            </div>
+            <div class="flex flex-row w-1/2 justify-between">
+              <div class="flex flex-row gap-x-1">
+                <div class="flex flex-col justify-between">
+                  <div></div>
+                  <div>Cost: $</div>
+                </div>
+                <div class="flex flex-col justify-between">
+                  <div></div>
+                  <input class="h-6 w-16  bg-neutral-100 border-b-2" style="outline:none"></div>
+              </div>
+              <div class="flex flex-row">
+                <div class="flex flex-col justify-between">
+                  <div></div>
+                  <div>Total: $</div>
+                </div>
+                <span class="flex flex-col justify-between">
+                  <div></div>
+                  <span></span>
+              </div>
+              <button class="flex h-full w-18 bg-black/30 rounded-md items-center
+               justify-center text-neutral-100 hover:bg-black/40">Create</button>
+            </div>
           </div>
-        </div>
-        <div class="flex flex-row">
-          <div class="flex flex-col w-full">
-            <label class="ml-1 text-sm text-neutral-600" for="inpDate">End date: </label>
-            <input
-              class="bg-neutral-100 p-1.5 rounded w-full border-1 border-neutral-200"
-              type="date"
-              id="inpDate"
-              name="trip-start"
-              min="2018-01-01"
-            />
+          <div class="flex flex-col h-full gap-y-3 mt-3">
+            <div>
+              <div class="text-xs font-bold">Work item:</div>
+              <div class="flex w-full h-8">
+                <input
+                  class="w-full h-full border-2 indent-2 bg-gray-400/10 border-gray-400/20 active:border-black"
+                  placeholder="Enter title of the work item"
+                >
+              </div>
+            </div>
+            <div>
+              <div class="text-xs font-bold">Description:</div>
+              <div class="flex w-full h-16">
+                <input
+                  class="flex w-full h-full place-content-start border-2 align-text-top indent-2 bg-gray-400/10 border-gray-400/20 active:border-black"
+                  placeholder="Write what this work item about"
+                >
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="flex flex-row">
-          <div class="flex flex-col w-full">
-            <label for="countries" class="ml-1 text-sm text-neutral-600">Select tag:</label>
-            <select
-              id="countries"
-              class="bg-neutral-100 p-1.5 rounded w-full border-1 border-neutral-200 focus:border-none"
-            >
-              <option selected>Choose a tag</option>
-              <option value="web">Web</option>
-              <option value="update">Update</option>
-              <option value="design">Design</option>
-              <option value="content">Content</option>
-            </select>
-          </div>
-        </div>
-        <div class="flex flex-row pt-2">
-          <button data-id="btnConfirm" class="bg-teal-600 text-white p-2 rounded-lg w-full font-bold">
-            ${this.#confirmText}
-          </button>
         </div>
       </div>
+    </div>
+  </div>
     `;
-    console.log('div.firstChild', div.children);
 
     const popup = div.children[0];
 
