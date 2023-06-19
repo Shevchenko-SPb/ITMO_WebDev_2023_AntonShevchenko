9b95fefdad0f6b4bd840fcf937e4d7325a32812c
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-06-19 12:54:55 +0300

    add open/close Popup

--- src/constants/dom.js
+++ src/constants/dom.js
@@ -1,12 +1,13 @@
 const Dom = {
   Button: {
     ADD_ITEM: 'btnAddItem',
+    CLOSE_POPUP: 'btnClosePopup',
   },
   Template: {
     TASK: 'templateTask!!!!!!!!!!!!!!!!!!',
   },
   Popup: {
-    CONTAINER: 'popupContainer!!!!!!!!!!!!!!!!!!!',
+    CONTAINER: 'popupAddItem',
   },
 };
 
