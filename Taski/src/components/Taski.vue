d8394c13fa06facc1fe6354eaa8938557d3a19c7
Shevchenko-SPb <Shevchenko-SPb@yandex.ru>
2023-07-08 21:58:10 +0300

    End labels and filters

--- Taski/src/components/Taski.vue
+++ Taski/src/components/Taski.vue
@@ -167,7 +167,7 @@
                         </btn>
                         <btn
                              class="text-grey-darken-1">
-                          <svg data-btn="btnDelete" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32"><path fill="currentColor" d="M12 12h2v12h-2zm6 0h2v12h-2z"/><path fill="currentColor" d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20zm4-26h8v2h-8z"/></svg>
+                          <svg data-btn="btnDelete"  xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32"><path fill="currentColor" d="M12 12h2v12h-2zm6 0h2v12h-2z"/><path fill="currentColor" d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20zm4-26h8v2h-8z"/></svg>
                         </btn>
                       </v-card-actions>
                     </v-card>
